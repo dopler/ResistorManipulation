b6c064cb942e12b34b8c3739f1297f580f83f742
Luis Luna <lluna@llu.edu>
2016-01-12 13:23:29 -0800

    basic 4 band resistor calculator

---
+++ Assets/zScripts/BandColorScript.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BandColorScript : MonoBehaviour 
+{
+
+	public List<Material> bandColors;
+	public int materialIndex;
+	public int bandNumber;
+
+	public double bandValue;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		materialIndex = 0;
+		this.GetComponent<MeshRenderer>().material = bandColors [materialIndex];
+		bandValue = 0;
+
+	}
+
+
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	public void Right()
+	{
+		materialIndex =  Mathf.Abs ((materialIndex + 1) % bandColors.Count);
+		this.GetComponent<MeshRenderer>().material = bandColors [materialIndex];
+
+		bandValue = GetBandValue (bandNumber, materialIndex);
+
+	}
+
+	public void Left()
+	{
+		if(materialIndex == 0)
+		{
+			materialIndex = bandColors.Count - 1;
+		}
+		else
+		{
+			materialIndex =  Mathf.Abs ((materialIndex - 1) % bandColors.Count);
+		}
+		this.GetComponent<MeshRenderer>().material = bandColors [materialIndex];
+		bandValue = GetBandValue (bandNumber, materialIndex);
+	}
+
+	public double GetBandValue(int bandNum, int index)
+	{
+		switch(bandNum)
+		{
+			case 1:
+				return index * 10;
+			case 2:
+				return index-1;
+			case 3:
+				if(index == 0)
+				{
+					return 0;
+				}
+				else if(index == 8)
+				{
+					return 0.1;
+				}
+				else if(index == 9)
+				{
+					return 0.01;
+				}
+
+				double total = 1.0;
+				for(int i = 0; i < index - 1; i++)
+				{
+					total *= 10;
+				}
+				return total;
+			case 4:
+				if(index == 0)
+				{
+					return 0;
+				}
+				else if(index == 1)
+				{
+					return 1;
+				}
+				else if(index == 2)
+				{
+					return 2;
+				}
+				else if(index == 3)
+				{
+					return 5;
+				}
+				else if(index == 4)
+				{
+					return 10;
+				}
+				return 0;
+			default:
+				return 0;
+		}
+
+	}
+}
---
+++ Assets/zScripts/GameController.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject band1;
+	public GameObject band2;
+	public GameObject band3;
+	public GameObject band4;
+
+	public Text value;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		string varianceTemp = GetVariance ();
+		value.text = CalculateDisplayValue () + "% Ohms"  + "~" + varianceTemp ;
+	}
+
+	string GetVariance ()
+	{
+		return band4.GetComponent<BandColorScript> ().bandValue.ToString ();
+	}
+
+	string CalculateDisplayValue()
+	{
+		double temp = band1.GetComponent<BandColorScript>().bandValue + band2.GetComponent<BandColorScript>().bandValue;
+		//temp = temp * band3.GetComponent<BandColorScript> ().bandValue;
+		if(band3.GetComponent<BandColorScript>().bandValue == 100)
+		{
+			return (temp/10).ToString() + "k";
+		}
+		if(band3.GetComponent<BandColorScript>().bandValue == 1000)
+		{
+			return temp.ToString() + "k";
+		}
+		else if(band3.GetComponent<BandColorScript>().bandValue == 10000)
+		{
+			return temp.ToString() + "0k";
+		}
+		else if(band3.GetComponent<BandColorScript>().bandValue == 100000)
+		{
+			return (temp/10).ToString() + "M";
+		}
+		else if(band3.GetComponent<BandColorScript>().bandValue == 1000000)
+		{
+			return temp.ToString() + "M";
+		}
+		else
+		{
+			temp = temp * band3.GetComponent<BandColorScript> ().bandValue;
+		}
+
+		return temp.ToString();
+	}
+}
